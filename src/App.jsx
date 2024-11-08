596efda4c441560de28f286ee9ce3e5bb8a9ec2d
ashwini894 <jadhavashwini2017@gmail.com>
2024-11-08 13:13:19 +0530

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import SignIn from './Pages/SignIn';
-import SignUp from './Pages/SignUp';
-import About from './Pages/About';
-import Profile from './Pages/Profile';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
